50c4b0e54b0a01499a5eec1fc451aa755ce97873
Farhan Tanmoy <43865401+ftanmoy@users.noreply.github.com>
2021-02-18 23:16:37 -0500

    part 2

---
+++ Pods/Target Support Files/Pods-FlixTix-FlixTixUITests/Pods-FlixTix-FlixTixUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlixTix_FlixTixUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FlixTix_FlixTixUITests
+@end
---
+++ Pods/Target Support Files/Pods-FlixTixTests/Pods-FlixTixTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlixTixTests : NSObject
+@end
+@implementation PodsDummy_Pods_FlixTixTests
+@end
